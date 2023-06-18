8a5cc6a157cebe0d4feb4149b31b492fe78b731a
itMegu <aljazkosem04@gmail.com>
2023-06-18 16:17:21 +0000

    pretvorba v axios pa useEffect za prikaz

--- tjulenjclient/src/vsebina/prikazPodatkov.js
+++ tjulenjclient/src/vsebina/prikazPodatkov.js
@@ -1,46 +1,45 @@
-import React, {Fragment, useEffect, useState} from "react"; 
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
 
 const PrikazPodatkov = () => {
-    const [podatki, setPodatki] = useState([]);
-    const getPodatki = async () => { 
-        try {
-            const resp = await fetch("http://192.168.50.5:5000/ustvaritev"); 
-            //parsing v mojo vednost
-            const jsonData = await resp.json(); 
-            setPodatki(jsonData);
-        } catch (err) {
-            console.log(err.message);
-        }
-    };
+  const [podatki, setPodatki] = useState([]);
 
-    useEffect(()=>{
-        getPodatki(); 
-    }, []);
+  useEffect(() => {
+  const getPodatki = async () => {
+    try {
+      const response = await axios.get('http://192.168.50.5:5000/ustvaritev');
+      setPodatki(response.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
+    getPodatki();
+  }, []);
 
-    return (
-        <Fragment>
-            <h1>Prikaz Test</h1>
-            <table className="table mt-3">
-                <thead>
-                <tr>
-                    <th>Ime</th>
-                    <th>Priimek</th>
-                    <th>Funkcija</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {podatki.map(podatek => (
-                        <tr>
-                            <td>{podatek.ime}</td>
-                            <td>{podatek.priimek}</td>
-                            <td>{podatek.funkcija}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </Fragment>
-    );
-}; 
+  return (
+    <Fragment>
+      <h1>Prikaz Test</h1>
+      <table className="table mt-3">
+        <thead>
+          <tr>
+            <th>Ime</th>
+            <th>Priimek</th>
+            <th>Funkcija</th>
+          </tr>
+        </thead>
+        <tbody>
+          {podatki.map((podatek, index) => (
+            <tr key={index}>
+              <td>{podatek.ime}</td>
+              <td>{podatek.priimek}</td>
+              <td>{podatek.funkcija}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
 
-export default PrikazPodatkov; 
+export default PrikazPodatkov;
